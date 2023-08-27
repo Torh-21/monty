44768ef6b0e0368298082d2539b9c369fb66d78a
Torh-21 <majarotoheeb@gmail.com>
2023-08-27 15:22:07 -0700

    pstr opcode added

--- pstr.c
+++ pstr.c
@@ -2,8 +2,10 @@
 /**
  * f_pstr - prints the string starting at the top of the stack,
  * followed by a new
+ *
  * @head: stack head
  * @counter: line_number
+ *
  * Return: no return
 */
 void f_pstr(stack_t **head, unsigned int counter)
